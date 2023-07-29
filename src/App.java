41578d4efcd0871f92768c9895b7e2c871bf12ff
Gustavo Baranda <baranda.gustavo@gmail.com>
2023-07-29 16:59:24 -0300

    Dialog Box - Main Menu

--- src/App.java
+++ src/App.java
@@ -1,5 +1,32 @@
+// Version: 1.0
+import javax.swing.JOptionPane;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Object[] options = {"Conversor de divisas", "Conversor de temperaturas", "Salir"};
+        String seleccion = (String) JOptionPane.showInputDialog(null, 
+            "Seleccione una opción", "Conversor", JOptionPane.QUESTION_MESSAGE, 
+            null, options, options[0]);
+
+        try {
+            if (seleccion.equals("Conversor de divisas")) {
+                convertirDivisas();
+            } else if (seleccion.equals("Conversor de temperaturas")) {
+                convertirTemperaturas();
+            } else {
+                JOptionPane.showMessageDialog(null, "Gracias por usar el conversor");
+                System.exit(0);
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Gracias por usar el conversor");
+            System.exit(0);
+        }    
     }
+
+    private static void convertirDivisas() {
+         
+    }
+    
+    private static void convertirTemperaturas() {
+
+    } 
 }

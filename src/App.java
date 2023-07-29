9f0cb83c58a307b1aba4185b611757e03446a518
Gustavo Baranda <baranda.gustavo@gmail.com>
2023-07-29 19:58:16 -0300

    currency conversion

--- src/App.java
+++ src/App.java
@@ -1,11 +1,15 @@
+
 // Version: 1.0
+import java.text.DecimalFormat;
+
 import javax.swing.JOptionPane;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        Object[] options = {"Conversor de divisas", "Conversor de temperaturas", "Salir"};
-        String seleccion = (String) JOptionPane.showInputDialog(null, 
-            "Seleccione una opción", "Conversor", JOptionPane.QUESTION_MESSAGE, 
-            null, options, options[0]);
+        Object[] options = { "Conversor de divisas", "Conversor de temperaturas", "Salir" };
+        String seleccion = (String) JOptionPane.showInputDialog(null,
+                "Seleccione una opción", "Conversor", JOptionPane.QUESTION_MESSAGE,
+                null, options, options[0]);
 
         try {
             if (seleccion.equals("Conversor de divisas")) {
@@ -19,14 +23,60 @@ public class App {
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, "Gracias por usar el conversor");
             System.exit(0);
-        }    
+        }
     }
 
     private static void convertirDivisas() {
-         
+        DecimalFormat df = new DecimalFormat("#.00");
+        boolean advance = true;
+        while (advance) {
+            String[] options = { "Pesos a Dólares", "Pesos a Euros", "Pesos a Libras esterlinas",
+                    "Pesos a Yen Japones", "Pesos a Won Sur Coreano" };// Opciones del menú
+
+            String seleccion = (String) JOptionPane.showInputDialog(null,
+                    "Seleccione una opción", "Conversor de divisas", JOptionPane.QUESTION_MESSAGE,
+                    null, options, options[0]);
+
+            String input = JOptionPane.showInputDialog(null,
+                    "Ingrese la cantidad de pesos a convertir");
+
+            double pesos = 0.0;
+
+            pesos = Double.parseDouble(input);
+            if (seleccion.equals("Pesos a Dólares")) {
+                double dolares = pesos / 20.0;
+                JOptionPane.showMessageDialog(null,
+                        pesos + " Pesos equivalen a " + df.format(dolares) + " Dólares");
+            } else if (seleccion.equals("Pesos a Euros")) {
+                double euros = pesos / 23.0;
+                JOptionPane.showMessageDialog(null,
+                        pesos + " Pesos equivalen a " + df.format(euros) + " Euros");
+            } else if (seleccion.equals("Pesos a Libras esterlinas")) {
+                double libras = pesos / 27.0;
+                JOptionPane.showMessageDialog(null,
+                        pesos + " Pesos equivalen a " + df.format(libras) + " Libras esterlinas");
+            } else if (seleccion.equals("Pesos a Yen Japones")) {
+                double yen = pesos / 0.18;
+                JOptionPane.showMessageDialog(null,
+                        pesos + " Pesos equivalen a " + df.format(yen) + " Yen japones");
+            } else {
+                double won = pesos / 0.017;
+                JOptionPane.showMessageDialog(null,
+                        pesos + " Pesos equivalen a " + df.format(won) + " Won sur coreano");
+            }
+
+            int choose = JOptionPane.showConfirmDialog(null,
+                    "¿Desea realizar otra conversión?", "Conversor de divisas",
+                    JOptionPane.YES_NO_OPTION);
+            if (choose == JOptionPane.NO_OPTION || choose == JOptionPane.CANCEL_OPTION) {
+                advance = false;
+                JOptionPane.showMessageDialog(null, "Gracias por usar el conversor");
+            }
+
+        }
     }
-    
+
     private static void convertirTemperaturas() {
 
-    } 
+    }
 }
